e6054263bf642ee6a7966b73b954114290037e77
nikhmatul hidayah <hinikhmatulhidayah@gmail.com>
2025-11-09 22:35:15 +0700

    logic cron views

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
+  Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+$interval = (int) env('THREAD_SYNC_INTERVAL_MINUTES', 60);
+
+if ($interval < 60) {
+    $cron = "*/{$interval} * * * *";
+} elseif ($interval % 60 === 0) {
+    $hours = $interval / 60;
+    $cron = "0 */{$hours} * * *";
+} else {
+    $minutes = $interval % 60;
+    $hours = floor($interval / 60);
+    $cron = "*/{$minutes} */{$hours} * * *";
+}
+
+Schedule::command('threads:sync-views')
+    ->cron($cron)
+    ->withoutOverlapping();
